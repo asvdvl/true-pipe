cdde0c34654407fd0ff34c64a62b69e4a44162a1
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-08-21 01:54:56 +0300

    Add project files.

---
+++ true pipe/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace true_pipe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.GroupBox groupBoxInputPipe;
+            System.Windows.Forms.GroupBox groupBoxOutputPipe;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label3;
+            this.textBoxArgumentsInputPipe = new System.Windows.Forms.TextBox();
+            this.textBoxExacutableFileInputPipe = new System.Windows.Forms.TextBox();
+            this.textBoxExacutableFileOutputPipe = new System.Windows.Forms.TextBox();
+            this.textBoxArgumentsOutputPipe = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            groupBoxInputPipe = new System.Windows.Forms.GroupBox();
+            groupBoxOutputPipe = new System.Windows.Forms.GroupBox();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            groupBoxInputPipe.SuspendLayout();
+            groupBoxOutputPipe.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(8, 19);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(20, 13);
+            label1.TabIndex = 1;
+            label1.Text = "file";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(159, 19);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(56, 13);
+            label2.TabIndex = 1;
+            label2.Text = "arguments";
+            // 
+            // groupBoxInputPipe
+            // 
+            groupBoxInputPipe.Controls.Add(label1);
+            groupBoxInputPipe.Controls.Add(label2);
+            groupBoxInputPipe.Controls.Add(this.textBoxArgumentsInputPipe);
+            groupBoxInputPipe.Controls.Add(this.textBoxExacutableFileInputPipe);
+            groupBoxInputPipe.Location = new System.Drawing.Point(4, 3);
+            groupBoxInputPipe.Name = "groupBoxInputPipe";
+            groupBoxInputPipe.Size = new System.Drawing.Size(666, 66);
+            groupBoxInputPipe.TabIndex = 2;
+            groupBoxInputPipe.TabStop = false;
+            groupBoxInputPipe.Text = "input pipe";
+            // 
+            // textBoxArgumentsInputPipe
+            // 
+            this.textBoxArgumentsInputPipe.Location = new System.Drawing.Point(161, 35);
+            this.textBoxArgumentsInputPipe.Name = "textBoxArgumentsInputPipe";
+            this.textBoxArgumentsInputPipe.Size = new System.Drawing.Size(499, 20);
+            this.textBoxArgumentsInputPipe.TabIndex = 1;
+            // 
+            // textBoxExacutableFileInputPipe
+            // 
+            this.textBoxExacutableFileInputPipe.Location = new System.Drawing.Point(8, 35);
+            this.textBoxExacutableFileInputPipe.Name = "textBoxExacutableFileInputPipe";
+            this.textBoxExacutableFileInputPipe.Size = new System.Drawing.Size(147, 20);
+            this.textBoxExacutableFileInputPipe.TabIndex = 0;
+            // 
+            // groupBoxOutputPipe
+            // 
+            groupBoxOutputPipe.Controls.Add(label4);
+            groupBoxOutputPipe.Controls.Add(label3);
+            groupBoxOutputPipe.Controls.Add(this.textBoxExacutableFileOutputPipe);
+            groupBoxOutputPipe.Controls.Add(this.textBoxArgumentsOutputPipe);
+            groupBoxOutputPipe.Location = new System.Drawing.Point(4, 69);
+            groupBoxOutputPipe.Name = "groupBoxOutputPipe";
+            groupBoxOutputPipe.Size = new System.Drawing.Size(666, 66);
+            groupBoxOutputPipe.TabIndex = 2;
+            groupBoxOutputPipe.TabStop = false;
+            groupBoxOutputPipe.Text = "output pipe";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(8, 18);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(20, 13);
+            label4.TabIndex = 1;
+            label4.Text = "file";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(159, 18);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(56, 13);
+            label3.TabIndex = 1;
+            label3.Text = "arguments";
+            // 
+            // textBoxExacutableFileOutputPipe
+            // 
+            this.textBoxExacutableFileOutputPipe.Location = new System.Drawing.Point(8, 34);
+            this.textBoxExacutableFileOutputPipe.Name = "textBoxExacutableFileOutputPipe";
+            this.textBoxExacutableFileOutputPipe.Size = new System.Drawing.Size(147, 20);
+            this.textBoxExacutableFileOutputPipe.TabIndex = 2;
+            // 
+            // textBoxArgumentsOutputPipe
+            // 
+            this.textBoxArgumentsOutputPipe.Location = new System.Drawing.Point(161, 34);
+            this.textBoxArgumentsOutputPipe.Name = "textBoxArgumentsOutputPipe";
+            this.textBoxArgumentsOutputPipe.Size = new System.Drawing.Size(499, 20);
+            this.textBoxArgumentsOutputPipe.TabIndex = 3;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(588, 142);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 172);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(groupBoxOutputPipe);
+            this.Controls.Add(groupBoxInputPipe);
+            this.Name = "Form1";
+            this.Text = "true pipe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            groupBoxInputPipe.ResumeLayout(false);
+            groupBoxInputPipe.PerformLayout();
+            groupBoxOutputPipe.ResumeLayout(false);
+            groupBoxOutputPipe.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxExacutableFileInputPipe;
+        private System.Windows.Forms.TextBox textBoxArgumentsInputPipe;
+        private System.Windows.Forms.TextBox textBoxExacutableFileOutputPipe;
+        private System.Windows.Forms.TextBox textBoxArgumentsOutputPipe;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ true pipe/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace true_pipe
+{
+    public partial class Form1 : Form
+    {
+        Process inputProcess;
+        Process outputProcess;
+        FileStream inputProcessStream;
+        FileStream outputProcessStream;
+        private SettingsFields settings = new SettingsFields();
+
+        private void LoadSettings() 
+        {
+            if (File.Exists(settings.XMLSettingsFileName))
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(SettingsFields));
+                TextReader reader = new StreamReader(settings.XMLSettingsFileName);
+                try
+                {
+                    settings = ser.Deserialize(reader) as SettingsFields;
+                    reader.Close();
+                }
+                catch (InvalidOperationException)
+                {
+                    reader.Close();
+
+                    if (File.Exists(settings.XMLSettingsFileName + ".old"))
+                    {
+                        File.Delete(settings.XMLSettingsFileName + ".old");
+                    }
+                    File.Copy(settings.XMLSettingsFileName, settings.XMLSettingsFileName + ".old");
+
+                    MessageBox.Show($"Файл с настройками поврежден! Старый файл сохранен как {Path.GetFileName(settings.XMLSettingsFileName)}.old");
+                    SaveSettings();
+                }
+            }
+        }
+
+        private void SaveSettings()
+        {
+            settings.InputFileName = textBoxExacutableFileInputPipe.Text;
+            settings.OutputFileName = textBoxExacutableFileOutputPipe.Text;
+            settings.InputArguments = textBoxArgumentsInputPipe.Text;
+            settings.OutputArguments = textBoxArgumentsOutputPipe.Text;
+
+            XmlSerializer ser = new XmlSerializer(typeof(SettingsFields));
+            TextWriter writer = new StreamWriter(settings.XMLSettingsFileName);
+
+            ser.Serialize(writer, settings);
+            writer.Flush();
+            writer.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+
+            inputProcess = new Process();
+            inputProcess.StartInfo.FileName = settings.InputFileName;
+            inputProcess.StartInfo.Arguments = settings.InputArguments;
+            inputProcess.StartInfo.UseShellExecute = false;
+            inputProcess.StartInfo.RedirectStandardOutput = true;
+            inputProcess.Start();
+
+            outputProcess = new Process();
+            outputProcess.StartInfo.FileName = settings.OutputFileName;
+            outputProcess.StartInfo.Arguments = settings.OutputArguments;
+            outputProcess.StartInfo.UseShellExecute = false;
+            outputProcess.StartInfo.RedirectStandardInput = true;
+            outputProcess.Start();
+
+            inputProcessStream = inputProcess.StandardOutput.BaseStream as FileStream;
+            outputProcessStream = outputProcess.StandardInput.BaseStream as FileStream;
+
+            int readLimit = 1 << 16;
+            int bufferSize = 1 << 8;
+            int lastRead;
+            byte[] data;
+
+            while (true)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] buffer = new byte[bufferSize];
+                    do
+                    {
+                        lastRead = inputProcessStream.Read(buffer, 0, buffer.Length);
+
+                        int readToEnd;
+                        while (lastRead < bufferSize && lastRead > 0)
+                        {
+                            readToEnd = inputProcessStream.Read(buffer, 0, buffer.Length - lastRead);
+                            if (readToEnd == 0)
+                                break;
+                            lastRead += readToEnd;
+                        }
+                        ms.Write(buffer, 0, lastRead);
+                    } while (lastRead > 0 && readLimit > ms.Length);
+                    data = ms.ToArray();
+                }
+
+                if (data.Length == 0)
+                {
+                    outputProcessStream.Flush();
+                    outputProcessStream.Close();
+                    outputProcess.WaitForExit();
+                    inputProcess.Close();
+                    break;
+                }
+
+                try
+                {
+                    outputProcessStream.Write(data, 0, data.Length);
+                }
+                catch (IOException)     //catch close channel
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSettings();
+            textBoxExacutableFileInputPipe.Text = settings.InputFileName;
+            textBoxExacutableFileOutputPipe.Text = settings.OutputFileName;
+            textBoxArgumentsInputPipe.Text = settings.InputArguments;
+            textBoxArgumentsOutputPipe.Text = settings.OutputArguments;
+        }
+    }
+
+    public class SettingsFields
+    {
+        public String XMLSettingsFileName = Environment.CurrentDirectory + "\\settings.xml";
+        public String InputFileName = "";
+        public String OutputFileName = "";
+        public String InputArguments = "";
+        public String OutputArguments = "";
+    }
+}
